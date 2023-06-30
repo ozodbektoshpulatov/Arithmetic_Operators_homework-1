8a88a5f39377157cd00906a07154c1c9b59c0532
ozodbektoshpulatov <toshpulatovozodbek96@gmail.com>
2023-06-30 10:34:51 +0500

    arithmetic 02

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,6 +1,6 @@
 #Create a varaible 'number' and assign it the value of 43.
-number=43
+number=23
 #Create a variable 'answer' and assign it the sum of the number's digits.
-answer=7
+answer=number//10+number%10
 #Print answer.
 print(answer)
